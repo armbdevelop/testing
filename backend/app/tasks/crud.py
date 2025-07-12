46da55570efc1280f9f090c8bcbb1eeed96bcebe
armbdevelop <armanbenyaminyan12@gmail.com>
2025-07-12 18:52:21 +0300

    backend: все 3 метода готовы, можно приступать к части фронтенда

--- backend/app/tasks/crud.py
+++ backend/app/tasks/crud.py
@@ -1,6 +1,7 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 from models import Task
+from tasks.schemas import TaskCreate, TaskUpdate
 
 
 class TaskCRUD:
@@ -12,3 +13,19 @@ class TaskCRUD:
             tasks: list[Task] = session.execute(select(Task).where(Task.is_done == False)).scalars().all()
         return tasks
 
+    def create_task(self, task: TaskCreate):
+        task_model = Task(name=task.name, description=task.description, is_done=False)
+
+        with self.session() as session:
+            session.add(task_model)
+            session.commit()
+            return task
+
+    def update_task(self, task: TaskUpdate):
+        q = update(Task).where(Task.id == task.id).values(is_done=True).returning(Task.name)
+
+        with self.session() as session:
+            task_name = session.execute(q).scalar_one_or_none()
+            session.commit()
+
+        return task_name
--- backend/app/tasks/views.py
+++ backend/app/tasks/views.py
@@ -4,6 +4,7 @@ from fastapi.params import Depends
 from dependencies import get_task_crud
 from models import Task
 from .crud import TaskCRUD
+from .schemas import TaskCreate, TaskUpdate
 
 router = APIRouter(prefix="/i_can_do_it", tags=["i_can_do_it"])
 
@@ -14,6 +15,9 @@ router = APIRouter(prefix="/i_can_do_it", tags=["i_can_do_it"])
 def get_active_task(
     taskC: Annotated[TaskCRUD, Depends(get_task_crud)],
 ):
+    """
+    Получение всех активных задач
+    """
     try:
         result = taskC.get_active_task()
         return {'result': result}
@@ -21,5 +25,37 @@ def get_active_task(
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@router.post(
+    "/create_task",
+)
+def create_task(
+    taskC: Annotated[TaskCRUD, Depends(get_task_crud)],
+    task: TaskCreate,
+):
+    """
+        Создание задачи, автоматом она будет считаться как активная
+    """
+    try:
+        result = taskC.create_task(task)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
+
+@router.patch(
+    "/update_task",
+)
+def update_task(
+    taskC: Annotated[TaskCRUD, Depends(get_task_crud)],
+    task: TaskUpdate,
+):
+    """
+    Обновляет задачу, делает ее выполненной
+    Возвращает имя задачи
+    """
+    try:
+        result = taskC.update_task(task)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
