6775647b90facfd43680d6b5acbf394d5494e5d3
armbdevelop <armanbenyaminyan12@gmail.com>
2025-07-12 19:21:35 +0300

    backend: бета 1.0

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,14 +9,13 @@ app = FastAPI()
 app.include_router(tasks_router)
 
 
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000, env_file='.env')
